ed06bfcd05898efa96dce7f91f00c7521f25cada
Ryan Atkinson <ratkinson@Ryans-MacBook-Pro-2.local>
2021-12-20 14:57:11 -0500

    prettier working

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 // TODO: theme
 const CYBER_YELLOW = '#FFEE00';
 
-const a = "b";
+const a = 'a';
 
 function App() {
   return (
-    <Box bg={CYBER_YELLOW} height="100vh">
-      <Flex direction="column" height="100%" align="center" justify="center">
-        <Text fontWeight="black" fontSize="7xl" >Atlas</Text>
-        <Text fontSize="3xl" pt={1}>Coming Soon</Text>
+    <Box bg={CYBER_YELLOW} height='100vh'>
+      <Flex direction='column' height='100%' align='center' justify='center'>
+        <Text fontWeight='black' fontSize='7xl'>
+          Atlas
+        </Text>
+        <Text fontSize='3xl' pt={1}>
+          Coming Soon
+        </Text>
       </Flex>
     </Box>
   );
--- src/mockData.js
+++ src/mockData.js
@@ -1,24 +1,26 @@
 export const MOCK_DATA = {
-    Main: {
-        1: {
-            site: 'www.google.com',
-        },
-        2: {
-            site: 'www.google.com',
-        },
-        3: {
-            site: 'www.google.com',
-        },
-        4: {
-            site: 'www.google.com',
-        },
-        5: {
-            site: 'www.google.com',
-        }
+  Main: {
+    1: {
+      site: 'www.google.com',
     },
-    ['Misc.']: {
-        1: {
-            site: 'www.google.com'
-        }
-    }
-}
+    2: {
+      site: 'www.google.com',
+    },
+    3: {
+      site: 'www.google.com',
+    },
+    4: {
+      site: 'www.google.com',
+    },
+    5: {
+      site: 'www.google.com',
+    },
+  },
+  ['Misc.']: {
+    1: {
+      site: 'www.google.com',
+    },
+  },
+};
+
+export default MOCK_DATA;
